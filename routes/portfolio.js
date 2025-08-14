bd9a14302231bed496abe860d90b3e2d4666be56
afiffudin <afiffudinm688@gmail.com>
2025-08-14 20:14:07 +0700

    update portfolio docker

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const portfolios = [
   { id: 1, title: "Tuntunan Sholat & Alquran Mobile", description: "Deskripsi proyek 1", image: "/images/Flutter1.png" },
   { id: 2, title: "IT Support & Network", description: "1. Instalasi CCTV, 2. Instalasi Networking (AP, Klimping Cable RJ45, Spliccing FO ), 3. Support Server ( Pemasangan Rack Server, Cable Mapping, Klimping LAN & FO ), 4. Support System & Hardware. ", image: "/images/Server_Network.png" },
-  { id: 3, title: "Proyek 3", description: "Deskripsi proyek 3", image: "/images/Flutter3.png" },
+  { id: 3, title: "Proyek 3", description: "Deskripsi proyek 3", image: "/images/docker1.png" },
 ];
 
 router.get('/', (req, res) => {

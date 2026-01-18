bdbd97802ce12e16c5d760ca92088b902522398c
afiffudin <afiffudinm688@gmail.com>
2026-01-18 21:54:35 +0700

    update IT Support, Mobile DEV, DEVOPS

--- app.js
+++ app.js
@@ -12,7 +12,22 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+// Route untuk halaman detail Programmer Web
+app.get('/portfolio/web', (req, res) => {
+    res.render('web-programmer'); // Sesuai nama file EJS yang dibuat tadi
+});
 
+app.get('/portfolio/devops', (req, res) => {
+  res.render('devops');
+});
+
+app.get('/portfolio/itsupport', (req, res) => {
+  res.render('it-support');
+});
+
+app.get('/portfolio/programming_mobile', (req, res) =>{
+  res.render('programming-mobile')
+} )
 // Routes
 app.get('/', (req, res) => {
   res.render('index', { title: 'Home' });
